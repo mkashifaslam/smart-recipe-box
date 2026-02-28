b2595ed65aa8d2a2d3ee323be157825eb85ab067
Kashif Aslam <mkashif.aslam36@gmail.com>
2026-02-28 15:09:10 +0500

    enhance recipe list styling and structure for improved readability

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,11 @@
-import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RecipeList } from './recipe-list/recipe-list';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.css',
-  imports: [RecipeList],
-  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [RecipeList]
 })
 export class App {
   protected readonly title = signal('Smart Recipe Book');
--- src/app/recipe-list/recipe-list.ts
+++ src/app/recipe-list/recipe-list.ts
@@ -4,9 +4,8 @@ import { MOCK_RECIPES } from '../mock-recipes';
 
 @Component({
   selector: 'app-recipe-list',
-  imports: [],
   templateUrl: './recipe-list.html',
-  styleUrl: './recipe-list.css',
+  styleUrl: './recipe-list.css'
 })
 export class RecipeList {
   recipes = signal<RecipeModel[]>(MOCK_RECIPES);
